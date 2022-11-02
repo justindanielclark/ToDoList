1d035501457a87a3b9309da44e2c0bbf20d8abd2
JClark <justindanielclark@gmail.com>
2022-11-02 16:40:59 -0700

    Changes to Asset Locations and Maps

---
+++ src/Assets/IconMap.js
@@ -0,0 +1,158 @@
+import approve from './SVGs/Icons/approve.svg'
+import backpack from './SVGs/Icons/backpack.svg'
+import bag from './SVGs/Icons/bag.svg'
+import beachSign from './SVGs/Icons/beachSign.svg'
+import beer from './SVGs/Icons/beer.svg'
+import bottle from './SVGs/Icons/bottle.svg'
+import bulb from './SVGs/Icons/bulb.svg'
+import burger from './SVGs/Icons/burger.svg'
+import cake from './SVGs/Icons/cake.svg'
+import camera from './SVGs/Icons/camera.svg'
+import cart from './SVGs/Icons/cart.svg'
+import chat from './SVGs/Icons/chat.svg'
+import chicken from './SVGs/Icons/chicken.svg'
+import coffe from './SVGs/Icons/coffe.svg'
+import computer from './SVGs/Icons/computer.svg'
+import diagram from './SVGs/Icons/diagram.svg'
+import donut from './SVGs/Icons/donut.svg'
+import download from './SVGs/Icons/download.svg'
+import favorite from './SVGs/Icons/favorite.svg'
+import flipflops from './SVGs/Icons/flipflops.svg'
+import flower from './SVGs/Icons/flower.svg'
+import forestCamp from './SVGs/Icons/forestCamp.svg'
+import forest from './SVGs/Icons/forest.svg'
+import gears from './SVGs/Icons/gears.svg'
+import headphones from './SVGs/Icons/headphones.svg'
+import hearts from './SVGs/Icons/hearts.svg'
+import heart from './SVGs/Icons/heart.svg'
+import home from './SVGs/Icons/home.svg'
+import hotdog from './SVGs/Icons/hotdog.svg'
+import hourglass from './SVGs/Icons/hourglass.svg'
+import icecream from './SVGs/Icons/icecream.svg'
+import information from './SVGs/Icons/information.svg'
+import key from './SVGs/Icons/key.svg'
+import kodakphoto from './SVGs/Icons/kodakphoto.svg'
+import lifeSaver from './SVGs/Icons/lifeSaver.svg'
+import lighthouse from './SVGs/Icons/lighthouse.svg'
+import location from './SVGs/Icons/location.svg'
+import luggage from './SVGs/Icons/luggage.svg'
+import mail from './SVGs/Icons/mail.svg'
+import mapLocation from './SVGs/Icons/mapLocation.svg'
+import martini from './SVGs/Icons/martini.svg'
+import message from './SVGs/Icons/message.svg'
+import microphone from './SVGs/Icons/microphone.svg'
+import milk from './SVGs/Icons/milk.svg'
+import mobile from './SVGs/Icons/mobile.svg'
+import paint from './SVGs/Icons/paint.svg'
+import passport from './SVGs/Icons/passport.svg'
+import penPaper from './SVGs/Icons/penPaper.svg'
+import people from './SVGs/Icons/people.svg'
+import photo from './SVGs/Icons/photo.svg'
+import pizza from './SVGs/Icons/pizza.svg'
+import play from './SVGs/Icons/play.svg'
+import plus from './SVGs/Icons/plus.svg'
+import ramen from './SVGs/Icons/ramen.svg'
+import repeat from './SVGs/Icons/repeat.svg'
+import reward from './SVGs/Icons/reward.svg'
+import science from './SVGs/Icons/science.svg'
+import search from './SVGs/Icons/search.svg'
+import shopping from './SVGs/Icons/shopping.svg'
+import signpost from './SVGs/Icons/signpost.svg'
+import snorkel from './SVGs/Icons/snorkel.svg'
+import sound from './SVGs/Icons/sound.svg'
+import stop from './SVGs/Icons/stop.svg'
+import taco from './SVGs/Icons/taco.svg'
+import tag from './SVGs/Icons/tag.svg'
+import target from './SVGs/Icons/target.svg'
+import thumbsUp from './SVGs/Icons/thumbsUp.svg'
+import thumsDown from './SVGs/Icons/thumsDown.svg'
+import time from './SVGs/Icons/time.svg'
+import trash from './SVGs/Icons/trash.svg'
+import tree from './SVGs/Icons/tree.svg'
+import upChevrons from './SVGs/Icons/upChevrons.svg'
+import upload from './SVGs/Icons/upload.svg'
+import visa from './SVGs/Icons/visa.svg'
+import volume from './SVGs/Icons/volume.svg'
+import wallet from './SVGs/Icons/wallet.svg'
+import wifi from './SVGs/Icons/wifi.svg'
+import world from './SVGs/Icons/world.svg'
+export default {
+  approve,
+  backpack,
+  bag,
+  beachSign,
+  beer,
+  bottle,
+  bulb,
+  burger,
+  cake,
+  camera,
+  cart,
+  chat,
+  chicken,
+  coffe,
+  computer,
+  diagram,
+  donut,
+  download,
+  favorite,
+  flipflops,
+  flower,
+  forestCamp,
+  forest,
+  gears,
+  headphones,
+  hearts,
+  heart,
+  home,
+  hotdog,
+  hourglass,
+  icecream,
+  information,
+  key,
+  kodakphoto,
+  lifeSaver,
+  lighthouse,
+  location,
+  luggage,
+  mail,
+  mapLocation,
+  martini,
+  message,
+  microphone,
+  milk,
+  mobile,
+  paint,
+  passport,
+  penPaper,
+  people,
+  photo,
+  pizza,
+  play,
+  plus,
+  ramen,
+  repeat,
+  reward,
+  science,
+  search,
+  shopping,
+  signpost,
+  snorkel,
+  sound,
+  stop,
+  taco,
+  tag,
+  target,
+  thumbsUp,
+  thumsDown,
+  time,
+  trash,
+  tree,
+  upChevrons,
+  upload,
+  visa,
+  volume,
+  wallet,
+  wifi,
+  world,
+}
---
+++ src/Assets/UserInterfaceIconMap.js
@@ -0,0 +1,24 @@
+import checked from './SVGs/UI/checked.svg';
+import close from './SVGs/UI/close.svg';
+import flag from './SVGs/UI/flag.svg';
+import garbage from './SVGs/UI/garbage.svg';
+import minus from './SVGs/UI/minus.svg';
+import newProject from './SVGs/UI/newProject.svg';
+import newTodo from './SVGs/UI/newTodo.svg';
+import pin from './SVGs/UI/pin.svg';
+import plus from './SVGs/UI/plus.svg';
+import viewAll from './SVGs/UI/viewAll.svg';
+
+
+export default {
+  checked,
+  close,
+  flag,
+  garbage,
+  minus,
+  newProject,
+  newTodo,
+  pin,
+  plus,
+  viewAll,
+}
